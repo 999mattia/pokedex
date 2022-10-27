49ef4e195993b7f5248b807ab23512c4f9675165
maettu999 <mattia.gisiger@gmail.com>
2022-10-27 11:08:26 +0200

    navigation

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,28 @@
 import styles from "../styles/Header.module.css"
 import Link from "next/link"
+import { useState, useEffect } from "react"
 
 export default function Header() {
+    const [menu, setMenu] = useState(false)
+
+    const handleClick = () => {
+        if (menu) {
+            setMenu(false)
+        } else {
+            setMenu(true)
+        }
+    }
+
     return (
-        <div className={styles.container}>
-            <center>
-                <Link href="/">
-                    <img src="/pokeball.svg" className={styles.pokeball} /></Link>
-            </center>
-        </div>
+        <>
+            <div className={styles.container}>
+                <center>
+                    {menu ? <><h3 className={styles.topLink}>Test</h3><h3>Test2</h3></> : null}
+                    <a onClick={() => handleClick()}>
+                        <img src="/pokeball.svg" className={styles.pokeball} /></a>
+                </center>
+            </div>
+
+        </>
     )
 }
